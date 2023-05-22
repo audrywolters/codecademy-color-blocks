9d8ebe14019798db652fc45a64c17aeac04aae0d
Audry Wolters <audrywolters@gmail.com>
2023-05-22 15:34:06 -0500

    refactored to make BlockList component

--- src/App.js
+++ src/App.js
@@ -1,41 +1,10 @@
-import React, { useState } from 'react'
-import Block from './Block'
-import './Block'
-
-const colorNames = ['Aquamarine', 'BlueViolet', 'Chartreuse', 'CornflowerBlue', 'Thistle', 'SpringGreen', 'SaddleBrown', 'PapayaWhip', 'MistyRose'];
+import React from 'react'
+import BlockList from './BlockList'
 
 function App() {
 
-  const [blockList, setBlockList] = useState([])
-
-  const handleClickNewBlock = () => {
-    setBlockList((prev) => {
-      return [{
-              id: Date.now(), 
-              color: colorNames[Math.floor(Math.random() * 9)],
-              handleClick: handleClickRemove
-              }, ...prev]
-    })
-  }
-
-  const handleClickRemove = (idToRemove) => {
-    setBlockList((prev) =>  prev.filter(
-      (block) => block.id !== idToRemove
-    ))
-  }
-
   return (
-    <>
-    <button onClick={handleClickNewBlock}>New Block</button>
-    <div className='blockContainer'>
-      {blockList.map((block) => (
-          <>
-            <div className='block' key={block.id} style={{backgroundColor: block.color}} onClick={() => block.handleClick(block.id)}></div> 
-            {/* <Block blockProps={block} /> */}
-          </>
-      ))}
-    </div>
-    </>
+    <BlockList />
   )
 }
 
--- src/BlockList.js
+++ src/BlockList.js
@@ -1,7 +1,37 @@
 import React, { useState } from 'react'
+import './Block.css'
+
+const colorNames = ['Aquamarine', 'BlueViolet', 'Chartreuse', 'DarkTurquoise', 'Thistle', 'SpringGreen', 'BurlyWood', 'PapayaWhip', 'MistyRose'];
 
 function BlockList() {
+	const [blockList, setBlockList] = useState([])
 
+	const handleClickNewBlock = () => {
+	  setBlockList((prev) => {
+		return [{
+				id: Date.now(), 
+				color: colorNames[Math.floor(Math.random() * 9)]
+				}, ...prev]
+	  })
+	}
+  
+	const handleClickRemove = (idToRemove) => {
+	  setBlockList((prev) =>  prev.filter(
+		(block) => block.id !== idToRemove
+	  ))
+	}
+
+	return (
+		<>
+			<button onClick={handleClickNewBlock}>New Block</button>
+		
+			<div className='blockContainer'>
+				{blockList.map((block) => (
+					<div className='block' key={block.id} style={{backgroundColor: block.color}} onClick={() => handleClickRemove(block.id)}></div>
+				))}
+			</div>
+		</>
+	)
 }
 
 export default BlockList
